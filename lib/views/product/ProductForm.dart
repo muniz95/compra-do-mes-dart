9beb3429bbc0d5fda33bbdd1455919a93d64e1f4
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-04-18 14:34:38 -0300

    Correção dos itens sobrescritos e duplicados

--- lib/views/product/ProductForm.dart
+++ lib/views/product/ProductForm.dart
@@ -1,32 +1,25 @@
 import 'dart:ui';
 
+import 'package:compra_do_mes/models/Product.dart';
 import 'package:compra_do_mes/redux/app/AppState.dart';
+import 'package:compra_do_mes/redux/product/ProductActions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-import 'ProductFormViewModel.dart';
+class ProductForm extends StatefulWidget {
 
-class ProductForm extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new StoreConnector<AppState, ProductFormViewModel>(
-      distinct: true,
-      converter: (store) => ProductFormViewModel.fromStore(store),
-      builder: (_, viewModel) => new ProductFormContent(viewModel: viewModel),
-    );
-  }
+  State<StatefulWidget> createState() => new ProductFormContent();
 }
 
-class ProductFormContent extends StatelessWidget {
+class ProductFormContent extends State<ProductForm> {
   final formKey = new GlobalKey<FormState>();
   final scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  ProductFormContent({
-    this.viewModel,
-  });
-
-  final ProductFormViewModel viewModel;
+  String _name;
+  double _price;
+  int _quantity;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +37,7 @@ class ProductFormContent extends StatelessWidget {
               new Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: new TextFormField(
-                  onSaved: (val) => viewModel.product.name = val,
+                  onSaved: (val) => _name = val,
                   validator: _validateProductName,
                   decoration: new InputDecoration(labelText: "Produto"),
                 ),
@@ -53,7 +46,7 @@ class ProductFormContent extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: new TextFormField(
                   keyboardType: TextInputType.number,
-                  onSaved: (val) => viewModel.product.price = double.parse(val),
+                  onSaved: (val) => _price = double.parse(val),
                   decoration: new InputDecoration(labelText: "Preço"),
                 ),
               ),
@@ -61,7 +54,7 @@ class ProductFormContent extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: new TextFormField(
                   keyboardType: TextInputType.number,
-                  onSaved: (val) => viewModel.product.quantity = int.parse(val),
+                  onSaved: (val) => _quantity = int.parse(val),
                   decoration: new InputDecoration(labelText: "Quantidade"),
                 ),
               ),
@@ -99,7 +92,10 @@ class ProductFormContent extends StatelessWidget {
 
     if (form.validate()) {
       form.save();
-      viewModel.addProduct(viewModel.product);
+      var store = StoreProvider.of<AppState>(context);
+      store.dispatch(new AddProductAction(
+        new Product(name: _name, price: _price, quantity: _quantity)
+      ));
       form.reset();
       _showSnackBar(context, 'Produto salvo com sucesso!');
     }
--- lib/views/product/ProductPage.dart
+++ lib/views/product/ProductPage.dart
@@ -1,28 +1,12 @@
 import 'package:compra_do_mes/constants/LoadingStatus.dart';
-import 'package:compra_do_mes/redux/app/AppState.dart';
 import 'package:compra_do_mes/views/product/ProductForm.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:compra_do_mes/views/common/ErrorView.dart';
 import 'package:compra_do_mes/views/common/LoadingView.dart';
 import 'package:compra_do_mes/views/common/PlatformAdaptativeProgressIndicator.dart';
-import 'ProductFormViewModel.dart';
 
 class ProductPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new StoreConnector<AppState, ProductFormViewModel>(
-      distinct: true,
-      converter: (store) => ProductFormViewModel.fromStore(store),
-      builder: (_, viewModel) => new ProductPageContent(viewModel),
-    );
-  }
-}
-
-class ProductPageContent extends StatelessWidget {
-  ProductPageContent(this.viewModel);
-  final ProductFormViewModel viewModel;
 
   @override
   Widget build(BuildContext context) {
